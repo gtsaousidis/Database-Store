13bf3ad37854c636ff10d7bded70d25b488e05a3
Dimitris Vranas <dimivranas@gmail.com>
2016-12-17 23:36:54 +0200

    Added Log Tag in MainActivity

--- app/src/main/java/com/database/android/databasestore/MainActivity.java
+++ app/src/main/java/com/database/android/databasestore/MainActivity.java
@@ -5,9 +5,14 @@ import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+    // LOG_TAG string for logging reasons
+    private static final String LOG_TAG = MainActivity.class.getSimpleName();
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
     }
 }
