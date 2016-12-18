0e1ede0aadbdd3349cc3ccbc179c84826f9e3872
Dimitris Vranas <dimivranas@gmail.com>
2016-12-18 23:54:19 +0200

    Added contract class for database

---
+++ app/src/main/java/com/database/android/databasestore/data/ItemContract.java
@@ -0,0 +1,37 @@
+package com.database.android.databasestore.data;
+
+import android.provider.BaseColumns;
+
+public final class ItemContract {
+
+    private ItemContract() {}
+
+    /**  Inner class that defines the first table from database */
+    public static class CustomersEntryProducts implements BaseColumns {
+
+        // The names of the columns in this table {customersProducts}
+        public static final String TABLE_NAME = "customersProducts";
+
+        public static final String COLUMN_ID = BaseColumns._ID;
+        public static final String COLUMN_NAME = "name";
+        public static final String COLUMN_ITEM_VARIETY = "variety";
+        public static final String COLUMN_ITEM_PRICE = "price";
+        public static final String COLUMN_ITEM_QUANTITY = "quantity";
+        public static final String COLUMN_ITEM_SUPPLIER = "supplier";
+        public static final String COLUMN_ITEM_PICTURE = "picture";
+    }
+
+    public static class ShopEntryContracts implements BaseColumns {
+
+        // The names of the columns in this table {ShopProducts}
+        public static final String TABLE_NAME = "shopProducts";
+
+        public static final String COLUMN_ID = BaseColumns._ID;
+        public static final String COLUMN_NAME = "name";
+        public static final String COLUMN_ITEM_VARIETY = "variety";
+        public static final String COLUMN_ITEM_PRICE = "price";
+        public static final String COLUMN_ITEM_QUANTITY = "quantity";
+        public static final String COLUMN_ITEM_SUPPLIER = "supplier";
+        public static final String COLUMN_ITEM_PICTURE = "picture";
+    }
+}
