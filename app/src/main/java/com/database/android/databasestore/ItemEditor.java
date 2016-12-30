5aa599273a45d2f945874d6ed4e5d211eb2dfb05
Dimitris Vranas <dimivranas@gmail.com>
2016-12-30 15:18:39 +0200

    Implemeted edit item activity when clicking an item of the list

--- app/src/main/java/com/database/android/databasestore/ItemEditor.java
+++ app/src/main/java/com/database/android/databasestore/ItemEditor.java
@@ -1,7 +1,9 @@
 package com.database.android.databasestore;
 
 import android.content.ContentValues;
+import android.content.CursorLoader;
 import android.content.Intent;
+import android.database.Cursor;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.net.Uri;
@@ -9,6 +11,7 @@ import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.NavUtils;
 import android.support.v7.app.AppCompatActivity;
+import android.text.TextUtils;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
@@ -23,9 +26,11 @@ import com.database.android.databasestore.data.ItemDbHelper;
 import java.io.FileNotFoundException;
 import java.io.InputStream;
 
-public class ItemEditor extends AppCompatActivity {
+public class ItemEditor extends AppCompatActivity implements android.app.LoaderManager.LoaderCallbacks<Cursor> {
 
     private final static int ACTIVITY_SELECT_IMAGE = 100;
+
+    private static final int ITEM_LOADER = 0;
 
     // EditText for entering name, variety, price and supplier
     private EditText mNameEditText, mVarietyEditText, mPriceEditText, mSupplierEditText, mQuantityText;
@@ -43,20 +48,25 @@ public class ItemEditor extends AppCompatActivity {
 
     private String path = "";
 
+    // The uri that is returned for a specific item frm the list through the intent's data
+    private Uri currentItemUri;
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_editor);
 
         Intent intent = getIntent();
-        Uri currentPetUri = intent.getData();
+        currentItemUri = intent.getData();
 
-        if (currentPetUri == null) {
+        if (currentItemUri == null) {
             // This is a new item so change the label to "Add an item"
             setTitle("Add an item");
         } else {
             // Otherwise this is an existing item so the label must be "Edit item"
             setTitle("Edit item");
+
+            getLoaderManager().initLoader(ITEM_LOADER, null, this);
         }
 
         // Find all the relevant views that is used to read input from user
@@ -78,43 +88,74 @@ public class ItemEditor extends AppCompatActivity {
         });
     }
 
-    private void insertPet() {
-
-        String productName = mNameEditText.getText().toString().trim();
-        String productVariety = mVarietyEditText.getText().toString().trim();
-        String productPriceString = mPriceEditText.getText().toString().trim();
-        int productPrice = Integer.parseInt(productPriceString);
-        String productSupplier = mSupplierEditText.getText().toString().trim();
-        String quantityProductString = mQuantityText.getText().toString().trim();
-        int productQuantity = Integer.parseInt(quantityProductString);
-
-        /**
-        Bitmap bitmap = ((BitmapDrawable)mImageView.getDrawable()).getBitmap();
-
-        ByteArrayOutputStream bos = new ByteArrayOutputStream();
-        bitmap.compress(Bitmap.CompressFormat.PNG, 100, bos);
-        byte[] img = bos.toByteArray();
-         */
+    private void savePet() {
 
         ContentValues values = new ContentValues();
-        values.put(ItemContract.CustomersEntryProducts.COLUMN_NAME, productName);
-        values.put(ItemContract.CustomersEntryProducts.COLUMN_ITEM_VARIETY, productVariety);
-        values.put(ItemContract.CustomersEntryProducts.COLUMN_ITEM_PRICE, productPrice);
-        values.put(ItemContract.CustomersEntryProducts.COLUMN_ITEM_SUPPLIER, productSupplier);
-        values.put(ItemContract.CustomersEntryProducts.COLUMN_ITEM_QUANTITY, productQuantity);
-        values.put(ItemContract.CustomersEntryProducts.COLUMN_ITEM_PICTURE, path);
+        if (currentItemUri == null) {
 
-        Uri newUri = getContentResolver().insert(ItemContract.CustomersEntryProducts.CONTENT_URI, values);
+            String productName = mNameEditText.getText().toString().trim();
+            String productVariety = mVarietyEditText.getText().toString().trim();
+            String productPriceString = mPriceEditText.getText().toString().trim();
+            int productPrice = 0;
+            if (!TextUtils.isEmpty(productPriceString)) {
+                productPrice = Integer.parseInt(productPriceString);
+            }
+            String productSupplier = mSupplierEditText.getText().toString().trim();
+            String quantityProductString = mQuantityText.getText().toString().trim();
+            int productQuantity = 0;
+            if (!TextUtils.isEmpty(quantityProductString)) {
+                productQuantity = Integer.parseInt(quantityProductString);
+            }
+            /**
+             Bitmap bitmap = ((BitmapDrawable)mImageView.getDrawable()).getBitmap();
 
-        // Show a toast message depending on whether or not the insertion was successful
-        if (newUri == null) {
-            // If the new content URI is null, then there was an error with insertion.
-            Toast.makeText(this, getString(R.string.editor_insert_item_failed),
-                    Toast.LENGTH_SHORT).show();
+             ByteArrayOutputStream bos = new ByteArrayOutputStream();
+             bitmap.compress(Bitmap.CompressFormat.PNG, 100, bos);
+             byte[] img = bos.toByteArray();
+             */
+
+            if (currentItemUri == null && TextUtils.isEmpty(productName)
+                    && TextUtils.isEmpty(productVariety) && TextUtils.isEmpty(productPriceString) &&
+                    TextUtils.isEmpty(productSupplier) && TextUtils.isEmpty(quantityProductString) &&
+                    TextUtils.isEmpty(path))
+            {return;}
+            values.put(ItemContract.CustomersEntryProducts.COLUMN_NAME, productName);
+            values.put(ItemContract.CustomersEntryProducts.COLUMN_ITEM_VARIETY, productVariety);
+            values.put(ItemContract.CustomersEntryProducts.COLUMN_ITEM_PRICE, productPrice);
+            values.put(ItemContract.CustomersEntryProducts.COLUMN_ITEM_SUPPLIER, productSupplier);
+            values.put(ItemContract.CustomersEntryProducts.COLUMN_ITEM_QUANTITY, productQuantity);
+            values.put(ItemContract.CustomersEntryProducts.COLUMN_ITEM_PICTURE, path);
+
+            Uri newUri = getContentResolver().insert(ItemContract.CustomersEntryProducts.CONTENT_URI, values);
+
+            // Show a toast message depending on whether or not the insertion was successful
+            if (newUri == null) {
+                // If the new content URI is null, then there was an error with insertion.
+                Toast.makeText(this, getString(R.string.editor_insert_item_failed),
+                        Toast.LENGTH_SHORT).show();
+            } else {
+                // Otherwise, the insertion was successful and we can display a toast.
+                Toast.makeText(this, getString(R.string.editor_insert_item_successful),
+                        Toast.LENGTH_SHORT).show();
+            }
         } else {
-            // Otherwise, the insertion was successful and we can display a toast.
-            Toast.makeText(this, getString(R.string.editor_insert_item_successful),
-                    Toast.LENGTH_SHORT).show();
+            // Otherwise this is an EXISTING item, so update the item with content URI: currentItemUri
+            // and pass in the new ContentValues. Pass in null for the selection and selection args
+            // because currentItemUri will already identify the correct row in the database that
+            // we want to modify.
+
+            int rowsAffected = getContentResolver().update(currentItemUri, values, null, null);
+
+            // Show a toast message depending on whether or not the update was successful.
+            if (rowsAffected == 0) {
+                // If no rows were affected, then there was an error with the update.
+                Toast.makeText(this, getString(R.string.editor_update_item_failed),
+                        Toast.LENGTH_SHORT).show();
+            } else {
+                // Otherwise, the update was successful and we can display a toast.
+                Toast.makeText(this, getString(R.string.editor_update_item_successfull),
+                        Toast.LENGTH_SHORT).show();
+            }
         }
     }
 
@@ -150,7 +191,7 @@ public class ItemEditor extends AppCompatActivity {
     public boolean onOptionsItemSelected(MenuItem item) {
         switch (item.getItemId()) {
             case R.id.action_save:
-                insertPet();
+                savePet();
                 finish();
             case R.id.action_delete:
                 return true;
@@ -160,5 +201,54 @@ public class ItemEditor extends AppCompatActivity {
                 return true;
         }
         return super.onOptionsItemSelected(item);
+    }
+
+
+    @Override
+    public android.content.Loader<Cursor> onCreateLoader(int i, Bundle bundle) {
+
+        String[] projection = {ItemContract.CustomersEntryProducts._ID,
+                ItemContract.CustomersEntryProducts.COLUMN_NAME,
+                ItemContract.CustomersEntryProducts.COLUMN_ITEM_VARIETY,
+                ItemContract.CustomersEntryProducts.COLUMN_ITEM_QUANTITY,
+                ItemContract.CustomersEntryProducts.COLUMN_ITEM_PRICE,
+                ItemContract.CustomersEntryProducts.COLUMN_ITEM_SUPPLIER,
+                ItemContract.CustomersEntryProducts.COLUMN_ITEM_PICTURE};
+
+
+        return new CursorLoader(this, currentItemUri, projection, null, null, null);
+    }
+
+    @Override
+    public void onLoadFinished(android.content.Loader<Cursor> loader, Cursor cursor) {
+
+        if (cursor.moveToFirst()) {
+            String name = cursor.getString(cursor.getColumnIndexOrThrow(ItemContract.CustomersEntryProducts.COLUMN_NAME));
+            String variety = cursor.getString(cursor.getColumnIndexOrThrow(ItemContract.CustomersEntryProducts.COLUMN_ITEM_VARIETY));
+            int quantity = cursor.getInt(cursor.getColumnIndexOrThrow(ItemContract.CustomersEntryProducts.COLUMN_ITEM_QUANTITY));
+            int price = cursor.getInt(cursor.getColumnIndexOrThrow(ItemContract.CustomersEntryProducts.COLUMN_ITEM_PRICE));
+            String supplier = cursor.getString(cursor.getColumnIndexOrThrow(ItemContract.CustomersEntryProducts.COLUMN_ITEM_SUPPLIER));
+            String imageString = cursor.getString(cursor.getColumnIndexOrThrow(ItemContract.CustomersEntryProducts.COLUMN_ITEM_PICTURE));
+
+            mNameEditText.setText(name);
+            mVarietyEditText.setText(variety);
+            mQuantityText.setText(Integer.toString(quantity));
+            mPriceEditText.setText(Integer.toString(price));
+            mSupplierEditText.setText(supplier);
+            if (imageString != null) {
+                Uri image = Uri.parse(imageString);
+                mImageView.setImageURI(image);
+            }
+        }
+    }
+
+    @Override
+    public void onLoaderReset(android.content.Loader<Cursor> loader) {
+            mNameEditText.setText(null);
+            mVarietyEditText.setText(null);
+            mQuantityText.setText(null);
+            mPriceEditText.setText(null);
+            mSupplierEditText.setText(null);
+            mImageView.setImageURI(null);
     }
 }
