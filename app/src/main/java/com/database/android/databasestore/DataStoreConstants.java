46b412b4506423f304a4b5e36f412cf041920a39
georgetsd <giorgostsd@gmail.com>
2017-10-22 21:04:26 +0300

    Adding datastore constants file.

---
+++ app/src/main/java/com/database/android/databasestore/DataStoreConstants.java
@@ -0,0 +1,15 @@
+package com.database.android.databasestore;
+
+/**
+ * Created by georgetsd on 10/22/17.
+ */
+
+public class DataStoreConstants {
+
+    public class ErrorStrings{
+        public final static String itemRequiresValisQuantity = "Item requires valid quantity";
+        public final static String itemRequiresValisPrice = "Item requires valid price!";
+        public final static String itemRequiresName = "Item requires a name";
+        public final static String itemMustHaveVarietyAndName = "Item must have a name and variety";
+    }
+}
